06d9c9595eb4b3d84a35212e001e68f42b5df8c5
Diane P <busybusyus@gmail.com>
2019-07-14 12:05:31 -0500

    completed assignment

--- main.py
+++ main.py
@@ -1,50 +1,65 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+import cgi
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-form ="""
-<!DOCTYPE html>
 
-<html>
-    <head>
-        <style>
-            form{{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}   
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;           
-            }}
-        </style>
-    </head>
-    <body>
-        <form action="/hello" method="post">
-            <label for="rotate-by">Rotate by:</label>
-                <input id="rotate-by" type=text name="rot" />      
-            <input type="submit" name="submit button" value="Submit"></input>
-        </form>
-        </body>
-</html> 
-"""
+@app.route("/invalid", methods=['POST'])
+def invalid():
+    Username = request.form['Username']
+    password = request.form['Password']
+    verifypassword = request.form['VerifyPassword']
+    email = request.form['Email']
 
-@app.route("/hello", methods=['POST'])
-def encrypt():
-    text = request.form['text']
-    num = int(request.form['rot'])
-    rotate=rotate_string(text,num)
-    return form.format(rotate)
+    usererror =""
+    passerror = ''
+    verifypasserror = ''
+    EmailError = ''
+
+    if Username == '':
+        usererror = 'Field is empty'
+    # or Password or VerifyPassword or Email == "":
+    
+    #if len(username) == " " in username
+    if len(Username) <3 or len(Username)>20:
+        usererror = "invalid submission"
+    
+    if password == '':
+        passerror = "Field is empty"
+   
+    if verifypassword == '':
+        verifypasserror = "Field is empty"
+
+    if password != verifypassword:
+        verifypasserror = "Passwords do not match."
+    
+    
+
+
+    if len(email) > 0:
+        
+        if len(email) < 3 or len(email)>20:
+            EmailError='Email does not have proper length'
+            
+    #    if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+    #        return True
+
+        elif email.count('@') != 1 or email.count('.') != 1:
+            EmailError='Not a valid Email address'
+
+    
+    if usererror == '' and passerror == '' and verifypasserror == '' and EmailError == '':
+        return "<h1>Welcome " + Username + " </h1>"  
+
+    return render_template('template.html', usererror = usererror, passerror=passerror, verifypasserror=verifypasserror, EmailError=EmailError, Username=Username)
+
     
 @app.route("/")
 def index():
-    return form.format(0)
+    return render_template('template.html')
 
 if __name__=='__main__':
     app.run()
